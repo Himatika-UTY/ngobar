487ee1d3c2eea9dcc222fea2d662c744e43e765a
selv <130154543+selverie@users.noreply.github.com>
2023-10-19 22:39:48 +0700

    Add files via upload

---
+++ pertemuan_1/function.py
@@ -0,0 +1,33 @@
+#sub program
+
+def Tambah():
+    x = 1
+    y = 2
+    hasil = 2 + 1
+    print(hasil)
+
+def Tambah2():
+    x = 1
+    y = 2
+    hasil = 2 + 1
+    print(hasil)
+
+
+
+def Tambah3():
+    x = 1
+    y = 2
+    hasil = 2 + 1
+    print(hasil)
+
+
+#program
+def main():
+    print("Hello World!")
+    Tambah()
+    Tambah2()
+    Tambah3()
+
+
+if __name__ == "__main__":
+    main()
---
+++ pertemuan_1/tipe data.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+#string
+a = str("Hello World")
+
+#integer
+b = int(20)
+
+#float
+c = float(20.5)
+
+#boolean
+d = bool(5 == 5)
+
+
+#konversi
+inte = int(c)
+
+
+animal = 10.2
+# animal2 = int("Kucing")
+
+print(d)
+
+
+
+
+animal = "kucing"
+
+hewan = ["kucing", "anjing", "gajah", 10, 10.2, True]
+tumbuhan = [True, 10]
+kendaraan = (1, 2, "hhh") #tuple
+
+user = {"Nomer" : 1,
+        "nama-barang" : "saos",
+        "harga" : 5000
+        }
+
+
+print(hewan)
+print(hewan[0])
+
+print(type(kendaraan))
+
+print(tumbuhan)
+print(kendaraan)
+
+print(user)
+print(user["nama-barang"])
+
+
+tesArray = np.array(["kuncing", "anjing"])
+
+print(type(tesArray))
+
